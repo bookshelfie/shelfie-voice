5164686d939e4335d8ee9facbf48e315f1e5bc1e
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-09-27 18:28:41 +0530

    move app definition outside main

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 from shelfie_voice import create_app
 
+
+application = create_app(config="config.base")
+
 if __name__ == "__main__":
-    application = create_app(config="config.base")
     application.run()
