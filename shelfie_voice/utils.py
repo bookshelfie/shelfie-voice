5ca7fd668c7166cbd6ae211ede78c5ee7fa124d5
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-10-07 20:12:40 +0530

    add utilities

---
+++ shelfie_voice/utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""utilities"""
+
+import requests
+from flask import current_app
+
+def find_books(book):
+    """Given a book's name, this function calls the API for the shelf."""
+    url = "{}/book/".format(current_app.config["SHELFIE_SERVER_URL"])
+    data = {
+        "title": book
+    }
+    response = requests.get(url, params=data)
+    return response.json()
+
+
+def find_books_by_author(author):
+    """Given an author's name, find all the books by him."""
+    url = "{}/author/".format(current_app.config["SHELFIE_SERVER_URL"])
+    data = {
+        "author": author
+    }
+    response = requests.get(url, params=data)
+    return response.json()
